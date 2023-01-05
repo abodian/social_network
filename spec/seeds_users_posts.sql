0d8dec41b305e597bd50d961c596708d022992a2
abodian <ajnbodian@gmail.com>
2023-01-05 14:17:34 +0000

    project completed

--- spec/seeds_users_posts.sql
+++ spec/seeds_users_posts.sql
@@ -1,4 +1,4 @@
-TRUNCATE TABLE users RESTART IDENTITY CASCADE; -- replace with your own table name.
+TRUNCATE TABLE users, posts RESTART IDENTITY; -- replace with your own table name.
 
 INSERT INTO users (username, user_email) VALUES ('David', 'david@gmail.com');
 INSERT INTO users (username, user_email) VALUES ('Anna', 'anna@gmail.com');
